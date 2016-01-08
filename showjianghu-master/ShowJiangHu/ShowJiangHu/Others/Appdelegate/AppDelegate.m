1c34f09035abb89a6bd4d7e3ff87440265fc988f
helantage <851962032@qq.com>
2016-01-08 14:09:08 +0800

    工程框架

--- showjianghu-master/ShowJiangHu/ShowJiangHu/Others/Appdelegate/AppDelegate.m
+++ showjianghu-master/ShowJiangHu/ShowJiangHu/Others/Appdelegate/AppDelegate.m
@@ -16,7 +16,13 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    self.window.backgroundColor = [UIColor redColor];
+    self.window.rootViewController = [[UIViewController alloc] init];
+    
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
