edff076af9662afb5f29f9da5c2bc0c2827cb4d8
helantage <851962032@qq.com>
2016-01-08 12:19:58 +0800

    1

--- showjianghu-master/ShowJiangHu/ShowJiangHu/ViewController.m
+++ showjianghu-master/ShowJiangHu/ShowJiangHu/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"oooo");
+    NSLog(@"我们");
 }
 
 - (void)didReceiveMemoryWarning {
